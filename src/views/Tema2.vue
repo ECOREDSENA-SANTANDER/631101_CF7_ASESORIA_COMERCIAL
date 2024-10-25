bd3d14d15c910e896fc3b99881e64371e6356e69
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2024-10-25 12:58:11 -0500

    3er Avance

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -2,10 +2,157 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-primario
       .titulo-principal__numero
         span 2
       h1 Relaciones con clientes
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-12.col-md-8.col-lg-4
+        .titulo-sexto.color-secundario.mb-4
+          h5 Figura 2. 
+          | Relaciones con clientes
+        img(src='@/assets/curso/temas/tema2/fig1.png', alt='La imagen simula un panal de abejas, y en diferentes celdas se lee: Precios, distribuidores, comerciantes, clientes, cadena de valor.')
+      .col-sm-12.col-md-12.col-lg-8
+        p.mb-4 La fidelización se debe entender cómo una acción de la empresa para lograr conseguir relaciones con los clientes que generen acciones redituables y continuadas con la empresa a lo largo del tiempo. 
+        .row.bgavatarazulclaro
+          p.mt-5.mb-5.ps-5 #[b Por parte de la empresa], para construir relaciones con los clientes se debe montar una estrategia conducente a que los proveedores, los clientes, distribuidores, comerciantes y públicos interesados mantengan intercambios de interés, precios y de calidad a lo largo del tiempo en la cadena de valor. 
+        .row.bgblanco.mt-3
+        .row.bgrosado
+          p.mt-5.mb-5.ps-5 #[b Por parte del vendedor], él debe estar en sintonía con los requerimientos de la empresa, ocupándose de resolver los problemas y las situaciones que se le presenten al cliente, puesto que esto representa valor económico y crecimiento personal para el vendedor.
+
+    p.mb-4 En la actualidad existen herramientas que contribuyen al mejoramiento de las relaciones con el cliente, lo que se denomina gestión de las relaciones, esta estrategia combina la tecnología de la información con la cultura organizacional, en la cual el principal objetivo es mantener excelentes relaciones y satisfacer las necesidades de los clientes. 
+
+    .subt.mb-4(data-aos="fade-right")
+      .d-flex.align-items-center.mb-1
+        img(src='@/assets/curso/temas/icono.svg', alt='')
+        h3.mb-0.ms-2 Protocolo de atención en la relación con el cliente
+    
+    p.mb-3 A continuación, lo puede consultar:
+
+    .row.justify-content-center.justify-content-center
+      .col-sm-12.col-lg-4.col-md-12.mb-4(data-aos="fade-right")
+        .overflow-hidden.h-100.p-4
+          .row.align-items-center.justify-content-center.bgfondotema2.p-4
+            img.img95(src='@/assets/curso/temas/tema2/tar1-1.png', alt='').mb-4
+            .row.justify-content-center.mb-3
+              .col-12
+                .bgmoradomedio.p-1 
+                  h3.text-center.mb-0 Definición
+            p.mb-5.text-center.p-4 Los protocolos de atención al cliente son la manera de fomentar en la organización la forma de actuar frente al cliente, el protocolo debe ser un documento que unifique los conceptos y criterios que lleven a cada colaborador a respetar y prestar una buena atención al cliente. Los protocolos deben destacar la imagen de la compañía y su identidad corporativa.
+      .col-sm-12.col-lg-4.col-md-12.mb-4(data-aos="fade-right")
+        .overflow-hidden.h-100.p-4
+          .row.align-items-center.justify-content-center.bgfondotema2.p-4
+            img.img95(src='@/assets/curso/temas/tema2/tar1-2.png', alt='').mb-4
+            .row.justify-content-center
+              .col-12
+                .bgmoradomedio.p-1 
+                  h3.text-center.mb-0 Importancia
+            p.text-center.p-4 La importancia que tiene un protocolo de atención y servicio al cliente radica en la manera cómo los asesores comerciales deben comportarse al atender a los clientes, explica la forma de actuar de los trabajadores en la atención al cliente o en el proceso de ventas, este documento detalla los procedimientos de interacción con el cliente, también debe contener los planes de mejora cuando se presenten hallazgos que desvíen la prestación del servicio.
+      .col-sm-12.col-lg-4.col-md-12.mb-4(data-aos="fade-right")
+        .overflow-hidden.h-100.p-4
+          .row.align-items-center.justify-content-center.bgfondotema2.p-4
+            img.img95(src='@/assets/curso/temas/tema2/tar1-2.png', alt='').mb-4
+            .row.justify-content-center.mb-3
+              .col-12
+                .bgmoradomedio.p-1 
+                  h3.text-center.mb-0 Manual
+            p.mb-4.text-center.p-4 El medio para lograrlo es elaborar un manual de protocolo de atención al cliente en forma clara y didáctica, comprensible para todos. Este manual debe explicar los procedimientos que se desea de los colaboradores, la conducta que deben tener con los usuarios o clientes y los procedimientos adecuados con relación a la atención que se quiere brindar en cualquier canal de contacto.
+
+    p.mb-5 Entre las ventajas que tiene para la empresa se mencionan las siguientes:
+
+    .row.align-items-center.justify-content-center.mb-5
+      .col-lg-8.p-0
+        .row.justify-content-center
+          .col-lg-3.sobavatar2.d-none.d-lg-block(data-aos="fade-right")
+            img(src="@/assets/curso/temas/tema2/avar1-1.png", alt="alt")
+          .col-lg-9(data-aos="fade-left")
+            .bgrosado2.borde-redondo-arriba.p-3
+              h3.moverderecha2.mt-4.mb-2 Atención robusta al cliente
+              p.moverderecha2.mb-4 Cada vez que se repite la misma acción entre los trabajadores, el servicio se perfecciona.
+        .row.justify-content-center
+          .col-lg-3.sobavatar2.d-none.d-lg-block(data-aos="fade-right")
+            img(src="@/assets/curso/temas/tema2/avar1-2.png", alt="alt")
+          .col-lg-9(data-aos="fade-left")
+            .bgmoradomedio.p-3
+              h3.moverderecha2.mt-4.mb-2 Procesos iguales en la atención al cliente
+              p.moverderecha2.mb-4 Para cada momento de encuentro con el cliente desde su llegada hasta en la postventa debe ser el mismo método de trabajo.
+        .row.justify-content-center
+          .col-lg-3.sobavatar2.d-none.d-lg-block(data-aos="fade-right")
+            img(src="@/assets/curso/temas/tema2/avar1-2.png", alt="alt")
+          .col-lg-9(data-aos="fade-left")
+            .bgrosado2.borde-redondo-abajo.p-3
+              h3.moverderecha2.mt-4.mb-2 Capacitación permanente
+              p.moverderecha2.mb-4 En cada uno de los procesos lo que permite un alto nivel de calidad.
+      .col-lg-4.p-0
+        img(src='@/assets/curso/temas/tema2/img1.png', alt='')
+
+    p.mb-5 Para la construcción del manual de servicio al cliente se debe tener en cuenta los siguientes elementos:
+
+    .row.align-items-center.justify-content-center.mb-5
+        .col-8.col-md-8.col-lg-4
+          img(src='@/assets/curso/temas/tema2/img2.png', alt='')
+        .col-sm-12.col-md-12.col-lg-8.mb-0.mt-4
+          .row.bgavatarazulclaro.ps-4.mb-0.p-3
+            ol.lista-ol--cuadro.lista-ol--separador.fa-ol.ps-3
+              li
+                .lista-ol--cuadro__vineta
+                  span 1
+                span.mb-0 Para cada cargo detallar sus funciones, responsabilidades y su línea de comunicación.
+              li.mb-0
+                .lista-ol--cuadro__vineta
+                  span 2
+                span.mb-0 Contener las instrucciones según las etapas del encuentro con el cliente.
+            ul.lista-ul--color.fa-al.ps-5.mb-0.mt-0
+                    li.mb-0 
+                      i.fas.fa-check-circle
+                      span.mb-0 #[b Inicio.] Ingreso del cliente al negocio, con qué saludo se recibirá, describir el mismo saludo para todos con respeto y de manera sonriente. Buenos días (tarde o noche) señor(a), mi nombre es…, bienvenido(a), (nombre de la empresa).      
+                    li.mb-0
+                      i.fas.fa-check-circle
+                      span.mb-0 #[b Identificación de la necesidad.] Mediante técnica AIDA u otra guiar al cliente en la solución de la necesidad.
+                    li.mb-1
+                      i.fas.fa-check-circle
+                      span.mb-0 #[b Conclusión y cierre.] Saludo de despedida y agradecimiento cuando el cliente ha satisfecho su necesidad.
+      
+    .subt.mb-4(data-aos="fade-right")
+      .d-flex.align-items-center.mb-1
+        img(src='@/assets/curso/temas/icono.svg', alt='')
+        h3.mb-0.ms-2 Ejemplo de protocolo de atención al cliente
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-sm-12.col-md-12.col-lg-8.mt-3
+        .row.bgrosado.mt-3
+          ul.lista-ul--color.fa-al.ps-5.mb-0.mt-4
+            li.mb-0 
+              i.fas.fa-check-circle
+              span.mb-0 #[b Saludo de bienvenida para el cliente.]
+            p.ps-4 Buenos días (tardes, noche), mi nombre es Alba en qué puedo ayudarlo. (Acompañado de un rostro amable).
+        .row.bgrosado.mt-3
+          ul.lista-ul--color.fa-al.ps-5.mb-0.mt-4.mb-4
+            li.mb-0 
+              i.fas.fa-check-circle
+              span.mb-0 Identificar su necesidad e invitarlo a realizar el recorrido al lugar de su necesidad.
+        .row.bgrosado.mt-3
+          ul.lista-ul--color.fa-al.ps-5.mb-0.mt-4.mb-4
+            li.mb-0 
+              i.fas.fa-check-circle
+              span.mb-0 Escuchar de manera atenta las inquietudes del cliente, haciendo preguntas hasta identificar su necesidad.
+        .row.bgrosado.mt-3
+          ul.lista-ul--color.fa-al.ps-5.mb-0.mt-4.mb-4
+            li.mb-0 
+              i.fas.fa-check-circle
+              span.mb-0 Dar la información clara sobre beneficios y ventajas del producto según su necesidad y, brindarle productos complementarios.
+        .row.bgrosado.mt-3
+          ul.lista-ul--color.fa-al.ps-5.mb-0.mt-4.mb-4
+            li.mb-0 
+              i.fas.fa-check-circle
+              span.mb-0 Dar despedida en forma cortés, agradecer la compra e invitarlo a regresar.
+      .col-8.col-md-8.col-lg-4
+        img(src='@/assets/curso/temas/tema2/img3.png', alt='')
+        
+        
+
+
 
 </template>
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -2,10 +2,52 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-primario
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Satisfacción al cliente
+
+    p La responsabilidad de las empresas en la satisfacción del cliente debe ser un objetivo de la calidad total, las organizaciones deben segmentar e identificar su cliente objetivo, esto les permitirá implementar el tipo de gestión determinada para cada negocio, determinando políticas y estrategias para cada proceso y la contribución de unas personas específicas.
+    p Para lograr la satisfacción total del cliente es necesario hacer lo siguiente:
+
+    .row.fondotema3.align-items-center.row.justify-content-center
+
+        .row.justify-content-center
+          .col-lg-10
+            PasosA.color-acento-contenido.mb-0(tipo="l")
+              .row.align-items-center.justify-content-center
+                .col-lg
+                  p.mb-0 Siempre hay que identificar las necesidades, deseos y expectativas de los clientes.
+                .col-auto
+                  figure(data-aos="zoom-in")
+                    img.d-none.d-lg-block(src='@/assets/curso/temas/tema3/img1-1.png', alt='')
+              .row.align-items-center.justify-content-center
+                .col-auto
+                  figure(data-aos="zoom-in")
+                    img.d-none.d-lg-block(src='@/assets/curso/temas/tema3/img1-2.png', alt='')
+                .col-lg
+                  p Planificar el servicio en función de las necesidades, manifestando y exponiendo al cliente las posibilidades existentes que tiene la organización comercial para cubrir sus necesidades y expectativas, cumpliendo con los requerimientos exigidos.
+              .row.align-items-center.justify-content-center
+                .col-lg
+                  p.mb-0 Se debe prestar el servicio de acuerdo con lo diseñado en función de los requerimientos y con los procesos acordados.
+                .col-auto
+                  figure(data-aos="zoom-in")
+                    img.d-none.d-lg-block(src='@/assets/curso/temas/tema3/img1-3.png', alt='')
+              .row.align-items-center.justify-content-center
+                .col-auto
+                  figure(data-aos="zoom-in")
+                    img.d-none.d-lg-block(src='@/assets/curso/temas/tema3/img1-4.png', alt='')
+                .col-lg
+                  p.mb-0 Se debe evaluar la prestación del servicio, teniendo en cuenta las medidas internas y externas a través de indicadores y parámetros del servicio de calidad.
+
+        .row.justify-content-center.mb-5.ps-5
+          .col-sm-12.col-md-12.col-lg-8
+            .row.mt-5
+              .cajon.color-acento-contenido.p-4.mb-4
+                p.p-3 NTC - ISO 9000-2015 es la norma técnica de gestión de la calidad vigente para el servicio al cliente. Las normas técnicas y de calidad están dirigidas a todas las entidades para mejorar el desempeño y la capacidad de proporcionar productos y servicios que cumplan con las necesidades y las expectativas del cliente.
+            p La empresa debe mejorar la atención al cliente, prestando un servicio de información y consulta a usuarios de acuerdo con sus requerimientos, teniendo en cuenta las políticas institucionales y los términos establecidos por la ley, esto hecho de manera personal o por medios tecnológicos, aplicando actitudes y valores mediante la comunicación empresarial y las normas de gestión documental, de acuerdo con los estándares de calidad que garantice la atención al cliente.
+          .col-12.col-md-8.col-lg-4
+            img.mb-5.mt-4(src='@/assets/curso/temas/tema3/imagen1.png', alt='')
 
 </template>
 
